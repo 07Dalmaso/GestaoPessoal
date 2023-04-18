6cb965d7f881ac1d5f3c0a7a03162a8de04035b5
Lucas Dalmaso <lucassdalmaso25@gmail.com>
2023-04-17 23:26:55 -0300

    ultimas modificações, antes da segunda apresentação

--- index/cartoes/cartoes.js
+++ index/cartoes/cartoes.js
@@ -9,6 +9,26 @@ function atualizarSaldo() {
   
     atualizarSaldo();
 
+    function atualizarSaldo1() {
+
+      var slider = document.getElementById("saldo1");
+      var saldo = document.getElementById("saldoAtual1");
+      saldo.innerHTML = 'R$ '+ " " + slider.value;
+  
+    }
+    
+      atualizarSaldo1();
+
+      function atualizarSaldo2() {
+
+        var slider = document.getElementById("saldo2");
+        var saldo = document.getElementById("saldoAtual2");
+        saldo.innerHTML = 'R$ '+ " " + slider.value;
+    
+      }
+      
+        atualizarSaldo2();
+
   
   
 
