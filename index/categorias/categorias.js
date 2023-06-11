54938c7e58dd56306753c269619027ca79b5e3b8
Joao Mothe <joao_mothe@hotmail.com>
2023-06-11 13:32:36 -0300

    NOVA FUNÇAO EM CATEGORIA

--- index/categorias/categorias.js
+++ index/categorias/categorias.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const categoriasBody = document.querySelector('#categorias-body');
+  const selectCategoria = document.querySelector('#select-categoria');
+  const transacoesTable = document.querySelector('#transacoes-categoria tbody');
 
   async function fetchTransactionsByCategory() {
     try {
@@ -28,11 +30,59 @@ document.addEventListener('DOMContentLoaded', () => {
         categoriaCell.textContent = categoria;
         row.appendChild(categoriaCell);
         const valorCell = document.createElement('td');
-        const formattedValue = categorias[categoria].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        const formattedValue = categorias[categoria].toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
         valorCell.textContent = formattedValue;
         row.appendChild(valorCell);
         categoriasBody.appendChild(row);
       }
+
+      selectCategoria.innerHTML = '<option value="">Selecione uma categoria</option>';
+      for (const categoria in categorias) {
+        const option = document.createElement('option');
+        option.value = categoria;
+        option.textContent = categoria;
+        selectCategoria.appendChild(option);
+      }
+
+      selectCategoria.addEventListener('change', () => {
+        const categoriaSelecionada = selectCategoria.value;
+        const transacoesFiltradas = transactions.filter(
+          (transaction) => transaction.category === categoriaSelecionada
+        );
+
+        transacoesTable.innerHTML = '';
+
+        if (transacoesFiltradas.length > 0) {
+          transacoesFiltradas.forEach((transaction) => {
+            const row = document.createElement('tr');
+            const dataCell = document.createElement('td');
+            const descricaoCell = document.createElement('td');
+            const valorCell = document.createElement('td');
+
+            dataCell.textContent = moment(transaction.date).format('DD/MM/YYYY');
+            descricaoCell.textContent = transaction.description;
+            valorCell.textContent = transaction.value.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            });
+
+            row.appendChild(dataCell);
+            row.appendChild(descricaoCell);
+            row.appendChild(valorCell);
+            transacoesTable.appendChild(row);
+          });
+        } else {
+          const row = document.createElement('tr');
+          const noDataCell = document.createElement('td');
+          noDataCell.setAttribute('colspan', '3');
+          noDataCell.textContent = 'Nenhuma transação encontrada para essa categoria.';
+          row.appendChild(noDataCell);
+          transacoesTable.appendChild(row);
+        }
+      });
     } catch (error) {
       console.error('Erro ao buscar transações:', error);
     }
