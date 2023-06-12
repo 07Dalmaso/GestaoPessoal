858ea53224d4a206bd86f78cc8594c1af9696799
Joao Mothe <joao_mothe@hotmail.com>
2023-06-12 15:47:36 -0300

    Pagina para as receitas

---
+++ assets/js/receitas.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formMongo = document.querySelector('#finance-form');
+    const tableBody = document.querySelector('#finance-table-body');
+  
+    formMongo.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      const date = moment(event.target.elements['finance-date'].value, 'YYYY-MM-DD').format('DD/MM/YYYY');
+      const description = event.target.elements['finance-description'].value;
+      const value = event.target.elements['finance-value'].value;
+  
+      try {
+        const response = await fetch('http://localhost:3000/finances', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ date, description, value }),
+        });
+  
+        if (response.ok) {
+          formMongo.reset();
+          formMongo.elements['finance-date'].focus();
+          updateFinanceList();
+        } else {
+          console.error('Erro ao adicionar receita:', response.status);
+        }
+      } catch (error) {
+        console.error('Erro ao adicionar receita:', error);
+      }
+    });
+  
+    async function updateFinanceList() {
+      try {
+        const response = await fetch('http://localhost:3000/finances');
+  
+        if (response.ok) {
+          const finances = await response.json();
+  
+          tableBody.innerHTML = '';
+  
+          finances.forEach((finance) => {
+            const row = document.createElement('tr');
+  
+            const dateCell = document.createElement('td');
+            dateCell.textContent = moment(finance.date).format('DD/MM/YYYY');
+            row.appendChild(dateCell);
+  
+            const descriptionCell = document.createElement('td');
+            descriptionCell.textContent = finance.description;
+            row.appendChild(descriptionCell);
+  
+            const valueCell = document.createElement('td');
+            valueCell.textContent = finance.value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            row.appendChild(valueCell);
+  
+            tableBody.appendChild(row);
+          });
+        } else {
+          console.error('Erro ao buscar receitas:', response.status);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar receitas:', error);
+      }
+    }
+  
+    updateFinanceList();
+  });
